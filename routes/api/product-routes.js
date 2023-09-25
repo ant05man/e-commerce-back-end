db98adcc7cb8423c9d463f09da0e529a15bb899f
Anthony Littlejohn <ant05man@yahoo.com>
2023-09-25 17:25:36 -0400

    created post route for Product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,16 +7,60 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll(
+    {
+      include: [{
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }]
+    }
+  )
+  .then(productData => res.json(productData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findOne({
+      where: {
+        id: req.params.id
+      },
+      include: [{
+        model: Category,
+        attributes: ['category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['tag_name']
+      }]
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No category found with that ID" });
+      return;
+    }
+
+    res.json(productData);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json(err);
+  }
+
 });
 
+
 // create new product
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   /* req.body should look like this...
     {
       product_name: "Basketball",
@@ -25,6 +69,18 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
+try {
+  const newProduct = await Product.create({
+    product_name: req.body.product_name
+  });
+
+  res.status(201).json(newProduct);
+} catch (err) {
+  console.error(err);
+  res.status(500).json({ error: "Internal server error" });
+}
+});
+
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
@@ -45,7 +101,6 @@ router.post('/', (req, res) => {
       console.log(err);
       res.status(400).json(err);
     });
-});
 
 // update product
 router.put('/:id', (req, res) => {
